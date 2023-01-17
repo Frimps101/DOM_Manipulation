ee264e606321cdda138de20acdd9568515dd13b4
Josephine Frimpomaa kwakye <jkwakye001@gmail.com>
2023-01-17 03:10:12 -0800

    Event Listeners

---
+++ Event Listeners/app.js
@@ -0,0 +1,10 @@
+let btn2 = document.querySelector(".btn-2")
+let btn3 = document.querySelector(".btn-3")
+
+btn2.addEventListener("click", ()=>{
+    alert("I love Python")
+})
+
+btn3.addEventListener("mouseover", ()=>{
+    btn3.style.backgroundColor = "blue";
+})
--- Favourite Movie Franchise/app.js
+++ Favourite Movie Franchise/app.js
@@ -42,3 +42,40 @@ console.log(firstListItem.textContent)
 
 // Modifying Attributes and Classes
 li.setAttribute("id", "main-heading")
+li.removeAttribute("id")
+
+const title = document.querySelector("#main-heading")
+console.log(title.getAttribute("id"))
+
+li.classList.add("list-items");
+li.classList.remove("list-items")
+li.classList.add("list-items");
+console.log(li.classList.contains("list-items"))
+
+// Remove Elements
+li.remove();
+
+// DOM traversal
+
+    // Parent Node Traversal
+let ul2 = document.querySelector("ul")
+console.log(ul2)
+console.log(ul2.parentNode.parentNode)
+console.log(ul2.parentElement.parentElement)
+
+const html = document.documentElement
+console.log(html)
+console.log(html.parentElement)
+
+    // Child Node Traversal
+console.log(ul.childNodes)
+console.log(ul2.firstChild)
+console.log(ul2.lastChild)
+
+ul2.childNodes[1].style.backgroundColor = 'blue'
+
+console.log(ul2.previousSibling)
+console.log(ul2.nextSibling)
+
+console.log(ul2.previousElementSibling)
+console.log(ul2.nextElementSibling)
